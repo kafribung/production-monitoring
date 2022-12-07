3dbd6cad72b4aa06b636d8005fb67f10f8c15cb6
Kafriansyah Kafriasnyah <kafri@prevalensi.com>
2022-12-07 20:47:57 +0700

    add reference table products

--- app/Models/ProductColor.php
+++ app/Models/ProductColor.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Location extends Model
+class ProductColor extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 }
--- database/migrations/2022_12_07_134522_create_product_sizes_table.php
+++ database/migrations/2022_12_07_134522_create_product_sizes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ return new class() extends Migration
      */
     public function up()
     {
-        Schema::create('product_categories', function (Blueprint $table) {
+        Schema::create('product_sizes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
 
@@ -38,6 +38,6 @@ return new class() extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('product_sizes');
     }
 };
