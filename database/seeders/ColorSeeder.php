0023b76b0936fb4f711dac9e93d73522e2b11fa8
Kafriansyah Kafriasnyah <kafri@prevalensi.com>
2023-02-02 12:15:25 +0700

    seeder color

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -14,18 +14,15 @@ class ColorSeeder extends Seeder
     public function run()
     {
         $details = collect([
-            'Biru',
-            'Hitam',
-            'Kuning',
-            'Merah',
-            'Hijau',
+            ['name' => 'Biru', 'hexa' => '#3159b3', 'created_by' => auth()->id() ?? 1],
+            ['name' => 'Hitam', 'hexa' => '#3159b3', 'created_by' => auth()->id() ?? 1],
+            ['name' => 'Kuning', 'hexa' => '#3159b3', 'created_by' => auth()->id() ?? 1],
+            ['name' => 'Merah', 'hexa' => '#3159b3', 'created_by' => auth()->id() ?? 1],
+            ['name' => 'Hijau', 'hexa' => '#3159b3', 'created_by' => auth()->id() ?? 1],
         ]);
 
         $details->each(function ($detail) {
-            \App\Models\Color::create([
-                'name' => $detail,
-                'created_by' => auth()->id() ?? 1,
-            ]);
+            \App\Models\Color::create($detail);
         });
     }
 }
