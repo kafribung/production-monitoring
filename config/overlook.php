98f641a958899132d3356e381a7b8afc3075c89e
Kafriansyah Kafriasnyah <kafri@prevalensi.com>
2023-04-04 23:43:18 +0700

    add widget and overlook

---
+++ config/overlook.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'includes' => [
+        App\Filament\Resources\UserResource::class,
+        App\Filament\Resources\ProductResource::class,
+    ],
+    'excludes' => [],
+];
