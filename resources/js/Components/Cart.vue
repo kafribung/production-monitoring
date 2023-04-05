ab013c7c05537aea99d1d8269872bcc829493b5f
Kafriansyah Kafriasnyah <kafri@prevalensi.com>
2023-04-06 00:57:28 +0700

    logout

--- resources/js/Components/Cart.vue
+++ resources/js/Components/Cart.vue
@@ -137,8 +137,9 @@ const url_img = location.origin + '/storage/'
                     <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
                     </MenuItem>
                     <MenuItem v-slot="{ active }">
-                    <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign
-                        out</a>
+                    <Link :href="route('logout')" method="post" as="a"
+                        :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign
+                    out</Link>
                     </MenuItem>
                 </MenuItems>
             </transition>
